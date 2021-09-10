02f1ef1a3be048217c422b100f7a930a78a2059f
Nadson <contato.nadson10@gmail.com>
2021-09-09 23:25:11 -0300

    Heroku

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  secret: process.env.NODE_ENV === "production" ? process.env.SECRET : "FY78ASDF9ADS5G9DF675GS9FGS9D734Y958734H98UT08WY34GF78SD0FG6D0SF70SDFGY45254DFG",
+  secret: process.env.NODE_ENV === "production" ? "FY78ASDF9ADS5G9DF675GS9FGS9D734Y958734H98UT08WY34GF78SD0FG6D0SF70SDFGY45254DFG" : "FY78ASDF9ADS5G9DF675GS9FGS9D734Y958734H98UT08WY34GF78SD0FG6D0SF70SDFGY45254DFG",
   api: process.env.NODE_ENV === "production" ? "https://ecommerce-api-tcc.herokuapp.com/" : "http://localhost:3000",
   loja: process.env.NODE_ENV === "proction" ? "http://localhost:8000" : "http://localhost:8000"
 };
