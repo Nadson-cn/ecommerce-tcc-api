aa23d9b9933f88bb1c9d74f7211d2b101d8eb34a
Nadson <contato.nadson10@gmail.com>
2021-09-24 00:04:51 -0300

    Atualiza especificacoes

--- controllers/validacoes/fichaValidation.js
+++ controllers/validacoes/fichaValidation.js
@@ -27,20 +27,8 @@ const FichaValidation = {
         especificacoes: Joi.array().items(Joi.string()).optional(),
         baias: Joi.array().items(Joi.string()).optional(), 
         placa_mae: Joi.array().items(Joi.string()).optional(), 
-        dimensoes: Joi.string().required(),
+        dimensoes: Joi.string().optional(),
     }},
-
-    update: {
-      params:{
-        id: Joi.string().required()
-      },
-      body: {
-        especificacoes: Joi.array().items(Joi.string()).optional(),
-        baias: Joi.array().items(Joi.string()).optional(), 
-        placa_mae: Joi.array().items(Joi.string()).optional(), 
-        dimensoes: Joi.string().required(),
-      }
-    },
 
   remove: {
     params: {
